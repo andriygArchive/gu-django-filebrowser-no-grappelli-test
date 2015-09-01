dfc1c6c4b6635e68aa0a5c55e8641586334c549a
Andriy Gushuley <agushuley@me.com>
2015-09-01 23:30:11 +0300

    #3 functions.get_version_path have guess paths also for files, which are not accessible on filesystem

--- app/tests/tests_functions.py
+++ app/tests/tests_functions.py
@@ -11,12 +11,13 @@ class FunctionsTest(TestCase):
     def __init__(self, methodName='runTest'):
         super(TestCase, self).__init__(methodName)
 
-    def test_get_version_path(self):
+    def setUp(self):
         shutil.rmtree(os.path.join(settings.MEDIA_ROOT, "2"), True)
         os.mkdir(os.path.join(settings.MEDIA_ROOT, "2"))
         shutil.copy(os.path.join(settings.MEDIA_ROOT, "rss.gif"),
                     os.path.join(settings.MEDIA_ROOT, "2"))
 
+    def test_get_version_path(self):
         for version in settings.FILEBROWSER_VERSIONS:
             version_generator("2/rss.gif", version)
 
@@ -41,6 +42,32 @@ class FunctionsTest(TestCase):
             self.assertTrue(orig_path.endswith(ends),
                             "Path [%s] is not ends with [%s]" % (path2, ends,))
             orig_path = get_version_path(path)
+            print("Path [%s] have to ends with [%s], version [%s->]\n" % (orig_path, ends, version))
+            self.assertTrue(orig_path.endswith(ends),
+                            "Path [%s] is not ends with [%s]" % (path2, ends,))
+
+    def test_get_version_path_do_not_check_file(self):
+        for version in settings.FILEBROWSER_VERSIONS:
+            path = get_version_path("3/rss.gif", version, check_file=False)
+            ends = "3/rss_" + version + ".gif"
+            print("Path [%s] have to ends with [%s], version [%s]\n" % (path, ends, version))
+            self.assertTrue(path.endswith(ends),
+                            "Path [%s] is not ends with [%s]" % (path, ends,))
+
+            for version2 in settings.FILEBROWSER_VERSIONS:
+                path2 = get_version_path(path, version2, check_file=False)
+                ends = "3/rss_" + version2 + ".gif"
+
+                print("Path [%s] have to ends with [%s], version [%s->%s]\n" % (path2, ends, version, version2))
+                self.assertTrue(path2.endswith(ends),
+                                "Path [%s] is not ends with [%s]" % (path2, ends,))
+
+            ends = "3/rss.gif"
+            orig_path = get_version_path(path, version_prefix='', check_file=False)
+            print("Path [%s] have to ends with [%s], version [%s->]\n" % (orig_path, ends, version))
+            self.assertTrue(orig_path.endswith(ends),
+                            "Path [%s] is not ends with [%s]" % (path2, ends,))
+            orig_path = get_version_path(path, check_file=False)
             print("Path [%s] have to ends with [%s], version [%s->]\n" % (orig_path, ends, version))
             self.assertTrue(orig_path.endswith(ends),
                             "Path [%s] is not ends with [%s]" % (path2, ends,))
